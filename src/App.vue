1651ed60cd56473dc47dca2428b92ebf59a7d1b5
Muhammad Abdullah <mabdullah@ogoul.com>
2024-09-26 10:09:55 +0500

    Data concept and how to add dynamic data inside componet

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <h6>hello world</h6>
   <HelloWorld msg="hello world"/>
+  <h6>{{name}}</h6>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
 export default{
   name:"App",
+  data(){
+    return{
+      name:"Muhammad Abdullah Rauf"
+    }
+  },
   components:{
     HelloWorld,
   }
