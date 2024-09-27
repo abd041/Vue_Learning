37ba6fd2532bee602914fced64e7609ebcac7005
Muhammad Abdullah <mabdullah@ogoul.com>
2024-09-28 01:14:04 +0500

    html binding done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,23 @@
 <template>
   <h4>{{msg}}</h4>
+  <div v-html="channel"></div>
 </template>
 
 <script>
 export default{
   name:"HelloWorld",
+  data(){
+    return(
+      {
+         channel:"<b>hello this is channel</b>"
+      }
+     
+    )
+
+  },
   props:{
     msg:String,
+    
   }
 }
 </script>
