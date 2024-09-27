c48abcadfa64ec920f078ae8390765d1dd3d5dbf
Muhammad Abdullah <mabdullah@ogoul.com>
2024-09-28 01:16:15 +0500

    code formatting

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <template>
   <h6>hello world</h6>
-  <HelloWorld msg="hello world"/>
-  <h6>{{name}}</h6>
+  <HelloWorld msg="hello world" />
+  <h6>{{ name }}</h6>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
-export default{
-  name:"App",
-  data(){
-    return{
-      name:"Muhammad Abdullah Rauf"
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "Muhammad Abdullah Rauf"
     }
   },
-  components:{
+  components: {
     HelloWorld,
   }
 }
 </script>
-
-
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,23 @@
 <template>
-  <h4>{{msg}}</h4>
+  <h4>{{ msg }}</h4>
   <div v-html="channel"></div>
 </template>
 
 <script>
-export default{
-  name:"HelloWorld",
-  data(){
-    return(
+export default {
+  name: "HelloWorld",
+  data() {
+    return (
       {
-         channel:"<b>hello this is channel</b>"
+        channel: "<b>hello this is channel</b>"
       }
-     
+
     )
 
   },
-  props:{
-    msg:String,
-    
+  props: {
+    msg: String,
+
   }
 }
 </script>
