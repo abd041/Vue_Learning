1de0f8e55bfa05d0543cc8bcc1063ebee3f9f4fa
Muhammad Abdullah <mabdullah@ogoul.com>
2024-10-01 20:40:01 +0500

    conditional binding to classes

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
   <h6>hello world</h6>
   <HelloWorld msg="hello world" />
   <h6>{{ name }}</h6>
+  <h5 v-bind:id="headingId">Heading</h5>
+  <button v-bind:disabled="isDisabled">Button</button>
+  <h5 v-bind:class="isDisabled ? 'Hello' : ''">Button</h5>
 </template>
 
 <script>
@@ -10,7 +13,9 @@ export default {
   name: "App",
   data() {
     return {
-      name: "Muhammad Abdullah Rauf"
+      name: "Muhammad Abdullah Rauf",
+      headingId:"Heading Id",
+      isDisabled:true,
     }
   },
   components: {
@@ -18,3 +23,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.Hello{
+  background: rebeccapurple;
+}
+</style>
