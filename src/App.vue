bcbf30fdbe13a8f0bbaebb54bfb44c09075858c1
Muhammad Abdullah <mabdullah@ogoul.com>
2024-11-05 18:17:13 +0500

    methods completed

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
 <template>
-<h2 v-if="num === 0 ">The number is zero</h2>
-<h2 v-else>The number is not zero</h2>
-
-<div v-for="(name, index) in fullnames" :key="index">{{ name.name }}
-
-  <h4 v-for="(move , index) in name.movies" :key="index">
-          {{move }}
-  </h4>
-</div>
-
-
+  <h4>{{ add(1 , 2 ,4) }}</h4>
+  <h4>{{ add(8, 5 , 10) }}</h4>
 </template>
 
 <script>
@@ -18,28 +9,15 @@ export default {
   name: "App",
   data() {
     return {
-     num:1,
-     names:["name 1" , "name 2" , "name 3" , "name 4"],
-     fullnames :[{
-      name:"hello",
-      movies:["a1" , "a2" , "a3"]
-     },
-     {
-      name:"hello",
-      movies:["a1" , "a2" , "a3"]
-     }
-    
-    ]
     }
   },
+  methods :{
+  add(a,b,c){
+    return a + b + c
+  }
+  }
 }
 </script>
 
 <style scoped>
-.Hello{
-  background: rebeccapurple;
-}
-.Helloh{
-  background: red;
-}
 </style>
